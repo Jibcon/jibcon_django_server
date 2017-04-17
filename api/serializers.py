1c042367ee5c36b67789bf64ce129497287dddfb
pjo901018 <pjo901018@korea.ac.kr>
2017-04-17 17:23:48 +0900

    token까지 serializer에 추가

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,41 @@
 from rest_framework import serializers
 
 
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        from rest_framework.authtoken.models import Token
+        model = Token
+        fields = (
+                'key',
+                )
+
 class UserSerializer(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField('get_or_create_token')
+
     class Meta:
         from django.contrib.auth.models import User
         model = User
         fields = (
             'username',
             'email',
+            'token',
             # todo unicode
         # utf-8 = unicode(euckr, 'euc-kr').encode('utf-8')
 
             # 'first_name',
             # 'last_name',
                   )
+
+    def get_or_create_token(self, obj):
+        from rest_framework.authtoken.models import Token
+        token = Token.objects.get_or_create(user=self.instance)
+        print(token)
+
+        serializer = TokenSerializer(token[0])
+
+        return serializer.data
+
+
 
 class UserSignedSerializer(serializers.Serializer):
     signed = serializers.BooleanField(required=True)
--- api/views.py
+++ api/views.py
@@ -3,8 +3,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
 from api.serializers import UserSerializer
 
 
@@ -83,6 +81,8 @@ class UserSignUpOrIn(generics.CreateAPIView):
             user = User.objects.get(
                 username=request.data.get('username'),
             )
+            # token = Token.objects.create()
+
             serializer = self.serializer_class(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except User.DoesNotExist:
