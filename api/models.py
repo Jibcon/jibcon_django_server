c061576f3f057de9dcc4c950246fb8734d2651bf
pjo901018 <pjo901018@korea.ac.kr>
2017-08-08 21:14:14 +0900

    DeviceItem Model변경

--- api/models.py
+++ api/models.py
@@ -32,17 +32,39 @@ class House(models.Model):
     houseIntro = models.CharField(max_length=255, default="고양이가 있는 짠주의 자취방이에욥")
     houseLocation = models.CharField(max_length=255, default="서울시 성북구 안암동 1, 고려대학교 파이빌 91호")
 
-variableDeviceTypes = [
-    "에어컨",
-    "선풍기",
-    "전구",
-    "플러그",
-]
+DEVICENAME_CHOICES = (
+    ("led", "LED전등"),
+    ("ultra", "초음파감지기"),
+)
+
+DEVICECOM_CHOICES = (
+    ("smArts", "smArts"),
+)
+
+DEVICETYPE_CHOICES = (
+    ("bulb", "전등"),
+    ("sensor", "센서"),
+)
+
+DEVICEAENAME_CHOICES = (
+    ("ae-secondled", "ae-secondled"),
+    ("ae-jyp", "ae-jyp"),
+)
+
+DEVICECNTNAME_CHOICES = (
+    ("cnt-led", "cnt-led"), # _req _res 둘다만들기
+    ("cnt-ultra", "cnt-ultra"),
+)
 
 class Device(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    deviceCom = models.CharField(max_length=255,default="찬주 일렉트로닉스")
-    deviceName = models.CharField(max_length=255,default="동그라미플러그")
+    deviceCom = models.CharField(choices=DEVICECOM_CHOICES, max_length=255, default=DEVICECOM_CHOICES[0])
+    deviceName = models.CharField(choices=DEVICENAME_CHOICES, max_length=255, default=DEVICENAME_CHOICES[0])
+    deviceType = models.CharField(choices=DEVICETYPE_CHOICES, max_length=255, default=DEVICETYPE_CHOICES[0])
     deviceWifiAddr = models.CharField(max_length=255,default="127.0.0.0")
-    deviceType = models.CharField(max_length=255,default=variableDeviceTypes[0])
     deviceOnOffState = models.BooleanField(default=False)
+    subscribeOnOffState = models.BooleanField(default=False)
+    roomName = models.CharField(max_length=255,default="거실")
+    aeName = models.CharField(choices=DEVICEAENAME_CHOICES, max_length=255,default=DEVICEAENAME_CHOICES[0])
+    cntName = models.CharField(choices=DEVICECNTNAME_CHOICES, max_length=255,default=DEVICECNTNAME_CHOICES[0])
+    content = models.CharField(max_length=255,default="1")
